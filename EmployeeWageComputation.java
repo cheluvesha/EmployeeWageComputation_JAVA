74395038432a4f8db123be247bc7d877a27a087c
cheluvesha <cheluvesh.b@gmail.com>
2020-07-20 15:17:54 +0530

    Part time wage

--- EmployeeWageComputation.java
+++ EmployeeWageComputation.java
@@ -29,3 +29,22 @@ class DailyEmpWage{
 }
 }
 
+#UC-3
+class PartTimeWage{
+        public static void main(String args[]){
+                int partTime=0;
+                int check=(int)(Math.floor(Math.random()*10)%2);
+                int salary=0;
+                int empRatePerHour=20;
+                int empHours;
+                if(partTime==check){
+                        empHours=4;
+                 }
+                else{
+                        empHours=8;
+                }
+                salary=empRatePerHour*empHours;
+                 System.out.println(salary);
+}
+}
+

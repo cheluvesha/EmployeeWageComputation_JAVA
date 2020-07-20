6e1e9dbb132fd0903e854e38ce0fca1c401f23f1
cheluvesha <cheluvesh.b@gmail.com>
2020-07-20 15:12:58 +0530

    Daily employee wage

--- EmployeeWageComputation.java
+++ EmployeeWageComputation.java
@@ -1,1 +1,31 @@
-Welcome to EmployeeWage computation
+#UC-1
+import java.lang.*;
+class Attendance{
+        public static void main(String args[]){
+                int present=1;
+                int check=(int)(Math.floor(Math.random()*10)%2);
+                if(present==check)
+                System.out.println("Present");
+                else
+                System.out.println("Not Present");
+}
+}
+
+#UC-2
+class DailyEmpWage{
+        public static void main(String args[]){
+                int present=1;
+                int check=(int)(Math.floor(Math.random()*10)%2);
+                int salary=0;
+                int empRatePerHour=20;
+                int empHours=8;
+                if(present==check){
+                        salary=empRatePerHour*empHours;
+                        System.out.println(salary);
+                }
+                else{
+                        System.out.println(salary);
+                }
+}
+}
+

978a23a677321177a7296f8bf1a979194bad06b2
cheluvesha <cheluvesh.b@gmail.com>
2020-07-20 15:22:33 +0530

    Wages for Month

--- EmployeeWageComputation.java
+++ EmployeeWageComputation.java
@@ -70,5 +70,33 @@ class SwitchCaseWage{
 }
 }
 
+#UC-5
+class WageForMonth{
+        public static void main(String args[]){
+                int salary=0;
+                int empRatePerHour=20;
+                int empHours;
+                int numberOfWorkingDays=20;
+                int totalSalary=0;
+                for(int day=1;day<=numberOfWorkingDays;day++){
+                int check=(int)(Math.floor(Math.random()*10)%2);
+                switch(check){
+                case 0 :
+                        empHours=4;
+                        break;
+                case 1 :
+                        empHours=8;
+                        break;
+                default :
+                        empHours=0;
+                }
+                salary=empRatePerHour*empHours;
+                totalSalary=+salary;
+                 }
+                System.out.println("Total wage for 20 days: "+totalSalary);
+}
+}
+
+
 
 

15f2a46df62206129b76e051bfd69527cae57f95
cheluvesha <cheluvesh.b@gmail.com>
2020-07-20 15:02:12 +0530

    Attendance

--- EmployeeWageComputation.java
+++ EmployeeWageComputation.java
@@ -1,1 +1,13 @@
-Welcome to EmployeeWage computation
+#UC-1
+import java.lang.*;
+class Attendance{
+        public static void main(String args[]){
+                int present=1;
+                int check=(int)(Math.floor(Math.random()*10)%2);
+                if(present==check)
+                System.out.println("Present");
+                else
+                System.out.println("Not Present");
+}
+}
+

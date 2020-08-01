58a90fa32390529388cf8c727ac693fb9f4c07a9
CHELUVESHA <38749165+cheluvesha@users.noreply.github.com>
2020-08-01 17:31:53 +0530

    Wages till a condition is added.

--- EmployeeWageComputation.java
+++ EmployeeWageComputation.java
@@ -101,4 +101,37 @@ public static void main(String args[]){
 }
 }
 
+#UC-6
+class WagesTillCondition{
+        final static int isPartTime=0;
+        final static int isFullTime=1;
+public static void main(String args[]){
+        int hours=0;
+        int days=0;
+        int salary;
+        int empRatePerHour=20;
+        int empHours;
+        int totalSalary=0;
+        int totalHours=100;
+        int totalDays=20;
+        while(hours<=totalHours && days<totalDays){
+              int check=(int)(Math.floor(Math.random()*10)%2);
+                switch(check){
+                case isPartTime:
+                        empHours=4;
+                        break;
+                case isFullTime:
+                        empHours=8;
+                        break;
+                default :
+                        empHours=0;
+                }
+                hours+=empHours;
+                days+=1;
+                salary=empRatePerHour*empHours;
+                totalSalary=totalSalary+salary;
+  }
+                System.out.println(totalSalary);
+}
+}
 
